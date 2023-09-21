f8f54bbb75d6b83943564dc816a16cc16a891c50
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-09-21 19:23:57 +0300

    color definitions, font etc. Major additions were made such as

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-    colors: {},
+    // container: {
+    //   center: true,
+    //   screens: {
+    //     lg: '1140px',
+    //     xl: '1140px',
+    //     '2xl': '1140px'
+    //   }
+    // },
+    extend: {
+      fontFamily: {
+        jost: ['Jost', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif']
+      },
+       colors: {
+         'vege-white': '#EFF5F5',
+         'vege-grey': '#D6E4E5',
+         'vege-blue': '#497174',
+         'vege-black': '#112B3C',
+         'vege-red': '#EB6440',
+       },
+
+      // spacing: {
+      //   128: '32rem',
+      // },
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
