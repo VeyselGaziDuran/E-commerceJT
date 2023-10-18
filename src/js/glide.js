3fcc008daf29e1f7fd1f0232ee8a771906376bbb
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-10-19 02:16:03 +0300

    new arrivals => Glide.js
    new arrivals made interactive with Glide.js

--- src/js/glide.js
+++ src/js/glide.js
@@ -18,3 +18,24 @@ const config = {
 };
 
 new Glide('.product-carousel',config).mount();
+
+
+const config2 = {
+    type: 'carousel',
+    perView: 4,
+    gap:20,
+    autoplay: 2000,
+    breakpoints: {
+        1024: {
+            perView: 3
+        },
+        800: {
+            perView: 2
+        },
+        480: {
+            perView: 2
+        }
+    }
+};
+
+new Glide('.product-carousel2',config2).mount();
